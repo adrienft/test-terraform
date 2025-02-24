16335b54208bd11922f4831c1bc4a19121892640
adrienft <adrien.ft@hotmail.com>
2025-02-24 12:26:44 +0100

    Nouvelle version du module

--- resources.tf
+++ resources.tf
@@ -8,12 +8,21 @@ resource "docker_container" "nginx" {
 
   name  = "nginx-${count.index}"
   image = docker_image.nginx.image_id
-
   memory     = var.container_memory
   privileged = var.privileged
 
   ports {
     internal = 80
     external = var.starting_port + count.index
+  }
+
+  upload {
+    content = file("./files/nginx.conf")
+    file    = "/etc/nginx/conf.d/default.conf"
+  }
+
+  upload {
+    content = file("./files/index.html")
+    file    = "/usr/share/nginx/html/index.html"
   }
 }
